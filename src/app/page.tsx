8c8a815a7fe631726651dc689043c332a489b772
Diyansh <diyansh@gmail.com>
2025-07-01 15:12:47 +0530

    Sync the authentication and Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
 import { SignedOut, SignInButton, SignUpButton, SignedIn, UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>Home Page</h1>
